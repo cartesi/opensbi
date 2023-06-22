ad843d2b53206eda53c7fa55dcbf81d488871920
Marcelo Politzer <251334+mpolitzer@users.noreply.github.com>
2023-06-22 15:30:18 -0300

    platform: cartesi: initial port

---
+++ platform/cartesi/htif.c
@@ -0,0 +1,99 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2010-2020, The Regents of the University of California
+ * (Regents).  All Rights Reserved.
+ */
+
+#include <sbi/riscv_locks.h>
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_error.h>
+#include <sbi/sbi_init.h>
+#include <sbi/sbi_platform.h>
+#include <sbi/sbi_system.h>
+#include <sbi_utils/sys/htif.h>
+
+#define HTIF_DEV_SYSTEM		0
+#define HTIF_DEV_CONSOLE	1
+
+#define HTIF_CONSOLE_CMD_GETC	0
+#define HTIF_CONSOLE_CMD_PUTC	1
+
+#define TOHOST_DEV(dev) ((uint64_t)(dev) << 56)
+#define TOHOST_CMD(cmd) ((uint64_t)(cmd) << 56 >> 8)
+#define TOHOST_DATA(cmd) ((uint64_t)(cmd) << 16 >> 16)
+#define TOHOST_CMD_DATA(cmd_data) (cmd_data << 8 >> 8)
+#define TOHOST_DEV_CMD_DATA(dev, cmd, data) \
+  (TOHOST_DEV(dev) | TOHOST_CMD(cmd) | TOHOST_DATA(data))
+
+#define FROMHOST_DEV(fromhost_value) ((uint64_t)(fromhost_value) >> 56)
+#define FROMHOST_CMD(fromhost_value) ((uint64_t)(fromhost_value) << 8 >> 56)
+#define FROMHOST_DATA(fromhost_value) ((uint64_t)(fromhost_value) << 16 >> 16)
+
+typedef struct {
+	uint64_t tohost;
+	uint64_t fromhost;
+	uint64_t iconsole;
+	uint64_t iyield;
+} htif_t;
+volatile htif_t *htif = (void *)(0x40008000);
+
+static void cartesi_htif_putc(char ch)
+{
+	htif->fromhost = 0;
+	htif->tohost = TOHOST_DEV_CMD_DATA(HTIF_DEV_CONSOLE, HTIF_CONSOLE_CMD_PUTC, ch);
+	(void)htif->fromhost;
+}
+
+static int cartesi_htif_getc(void)
+{
+	htif->fromhost = 0;
+	htif->tohost = TOHOST_DEV_CMD_DATA(HTIF_DEV_CONSOLE, HTIF_CONSOLE_CMD_GETC, 0);
+	return (int)(FROMHOST_DATA(htif->fromhost))-1;
+}
+
+static struct sbi_console_device cartesi_console = {
+	.name = "htif",
+	.console_putc = cartesi_htif_putc,
+	.console_getc = cartesi_htif_getc
+};
+
+int cartesi_htif_serial_init()
+{
+	sbi_console_set_device(&cartesi_console);
+	return 0;
+}
+
+static int cartesi_htif_system_reset_check(u32 type, u32 reason)
+{
+	return 1;
+}
+
+static void cartesi_htif_system_reset(u32 type, u32 reason)
+{
+	while (1) {
+		htif->fromhost = 0;
+		htif->tohost = 1;
+	}
+}
+
+struct sbi_system_reset_device cartesi_reset = {
+	.name = "htif",
+	.system_reset_check = cartesi_htif_system_reset_check,
+	.system_reset = cartesi_htif_system_reset
+};
+
+int cartesi_htif_system_reset_init()
+{
+	sbi_system_reset_add_device(&cartesi_reset);
+	return 0;
+}
+
+uint64_t sbi_system_yield(uint64_t cmd_data)
+{
+	sbi_exit(sbi_scratch_thishart_ptr());
+
+	htif->fromhost = 0;
+	htif->tohost = TOHOST_DEV(2) | TOHOST_CMD_DATA(cmd_data);
+	return htif->fromhost;
+}
---
+++ platform/cartesi/platform.c
@@ -0,0 +1,75 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Western Digital Corporation or its affiliates.
+ */
+
+#include <sbi/riscv_asm.h>
+#include <sbi/riscv_encoding.h>
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_const.h>
+#include <sbi/sbi_platform.h>
+#include <sbi/sbi_system.h>
+#include <sbi_utils/fdt/fdt_domain.h>
+#include <sbi_utils/fdt/fdt_fixup.h>
+#include <sbi_utils/fdt/fdt_helper.h>
+#include <sbi_utils/fdt/fdt_pmu.h>
+#include <sbi_utils/ipi/fdt_ipi.h>
+#include <sbi_utils/irqchip/fdt_irqchip.h>
+#include <sbi_utils/reset/fdt_reset.h>
+#include <sbi_utils/sys/htif.h>
+#include <sbi_utils/timer/fdt_timer.h>
+
+
+int cartesi_htif_serial_init();
+int cartesi_htif_system_reset_init();
+
+static int platform_final_init(bool cold_boot)
+{
+	void *fdt;
+
+	if (cold_boot)
+		fdt_reset_init();
+
+	if (!cold_boot)
+		return 0;
+
+	fdt = fdt_get_address();
+
+	fdt_cpu_fixup(fdt);
+	fdt_fixups(fdt);
+	fdt_domain_fixup(fdt);
+
+	return cartesi_htif_system_reset_init();
+}
+
+static u64 platform_tlbr_flush_limit(void)
+{
+	return SBI_PLATFORM_TLB_RANGE_FLUSH_LIMIT_DEFAULT;
+}
+
+static int platform_pmu_init(void)
+{
+	return fdt_pmu_setup(fdt_get_address());
+}
+
+const struct sbi_platform_operations platform_ops = {
+	.final_init		= platform_final_init,
+	.console_init		= cartesi_htif_serial_init,
+	.timer_init		= fdt_timer_init,
+	.timer_exit		= fdt_timer_exit,
+	.ipi_init		= fdt_ipi_init,
+	.ipi_exit		= fdt_ipi_exit,
+	.get_tlbr_flush_limit	= platform_tlbr_flush_limit,
+	.pmu_init		= platform_pmu_init,
+};
+
+const struct sbi_platform platform = {
+	.opensbi_version	= OPENSBI_VERSION,
+	.platform_version	= SBI_PLATFORM_VERSION(0x0, 0x00),
+	.name			= "cartesi",
+	.features		= SBI_PLATFORM_DEFAULT_FEATURES,
+	.hart_count		= 1,
+	.hart_stack_size	= SBI_PLATFORM_DEFAULT_HART_STACK_SIZE,
+	.platform_ops_addr	= (unsigned long)&platform_ops
+};
